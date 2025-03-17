2b29650b59a3ec62fab9245e6d998b2a6a76a3c7
Shoxrux <shohruh@yandex.ru>
2025-03-17 20:58:47 +0500

    add database

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__ . '/public.php';
+require __DIR__ . '/customer.php';
+require __DIR__ . '/vendor.php';
+require __DIR__ . '/admin.php';
